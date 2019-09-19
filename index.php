bf37e7f049217353e8df9bc9d2ac963f78b669d5
kashyap2754 <48773264+kashyap2754@users.noreply.github.com>
2019-09-19 16:32:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-  echo 'Hello from ' . gethostname() . '!';
+   $db = pg_Connect("dbname=postgres user=postgres password=postgres");
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+   
+   $sql =<<<EOF
+      CREATE TABLE COMPANY
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      AGE            INT     NOT NULL,
+      ADDRESS        CHAR(50),
+      SALARY         REAL);
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret) {
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+   pg_close($db);
 ?>
